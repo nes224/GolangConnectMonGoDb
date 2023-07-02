7a06c9956ac8b3317046a1ff5ecedb9917d2f4d8
Nes224 <Nes224@hotmail.com>
2023-07-02 14:46:23 +0700

    second commit

--- main.go
+++ main.go
@@ -9,21 +9,26 @@ import (
 )
 // Set up the MongoDB connection options
 func main(){
-	clientOptions := options.Client().ApplyURI("mongodb+srv://<user>:<password>@cluster0.sclsoat.mongodb.net/?retryWrites=true&w=majority")
+  // Use the SetServerAPIOptions() method to set the Stable API version to 1
+  serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+  opts := options.Client().ApplyURI("mongodb+srv://<user>:<password>@cluster0.sclsoat.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
+  // Create a new client and connect to the server
+  client, err := mongo.Connect(context.TODO(), opts)
+  if err != nil {
+    panic(err)
+  }
+  defer func() {
+    if err = client.Disconnect(context.TODO()); err != nil {
+      panic(err)
+    }
+  }()
+  // Check the connection
+  err = client.Ping(context.TODO(),nil)
+  if err != nil{
+	fmt.Println("Failed to ping MongoDB:",err)
+	return
+  }
+  // Send a ping to confirm a successful connection
 
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		fmt.Println("Failed to connect to MongoDB:", err)
-		return
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		fmt.Println("Failed to ping MongoDB:", err)
-		return
-	}
-
-	fmt.Println("Connected to MongoDB!")
+  fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 }
